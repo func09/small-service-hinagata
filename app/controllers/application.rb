80835e78f2bc990f2a90fbf69e5eb8aff37dd25b
func09 <mitsuru.haga@gmail.com>
2009-03-06 04:03:30 +0900

    add authentication.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '7d11dbb2c07cf410d3db64edcb1c1296'
   
+  include AuthenticatedSystem
+  
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
