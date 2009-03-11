0fd77b40c459f3c8d6f1d6e23f94ba899ed2223d
芳賀 充 <mitsuru.haga@gmail.com>
2009-03-11 13:17:28 +0900

    Display flash messages in application layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # Return HTML what flash messages.
+  def flash_message
+    message = ""
+    [:notice,:info,:warning,:error].each do |type|
+      message << "<div id='flash-#{type}'>#{flash[type]}</div>\n"
+    end
+    message
+  end
+  
 end
