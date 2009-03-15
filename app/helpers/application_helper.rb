30a04e26a85b17aa3754a95fd06b5eaae79177fc
func09 <mitsuru.haga@gmail.com>
2009-03-16 00:42:59 +0900

    Add global menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  # Return true if the currently logged in user is an admin.
+  def admin?
+    logged_in? && current_user.has_role?(:admin)
+  end
+  
   # Return HTML what flash messages.
   def flash_message
     message = ""
